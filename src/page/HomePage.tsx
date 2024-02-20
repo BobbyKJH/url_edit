b610eb903e6feb8e136a982d88e2cfe6018cb15a
bobby <rlatnguscjsw@gmail.com>
2024-02-20 21:24:21 +0900

    Feat : URL Copy
    - URL Query 제거 후 -> URL Copy 후 -> Open

--- src/page/HomePage.tsx
+++ src/page/HomePage.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
-import { HomaPagePaper } from "@/page/HomePage.styled"
+/** Style */
 import { Button, TextField } from "@mui/material";
+import { HomaPagePaper } from "@/page/HomePage.styled"
 
 const HomePage: React.FC = () => {
   const [url, setUrl] = useState("");
@@ -10,19 +11,28 @@ const HomePage: React.FC = () => {
     setUrl(value);
   };
 
+  /** URL Query 제거 기능 */
   const handleUrlEdit = (event: React.FormEvent) => {
     event.preventDefault();
     if(url.includes("?")){
       setUrl(url.substring(0, url.indexOf("?", 0)))
     } else {
-      console.log("hello")
+      alert("query가 존재하지 않습니다.")
     }
   }
 
   const handleLink = (event: React.FormEvent) => {
     event.preventDefault();
-    window.open(url);
-
+    if(!url.includes("?")){
+      /** URL Query 복사 기능 */
+      window.navigator.clipboard.writeText(url)
+      /** URL OPEN */
+      setTimeout(() => {
+        window.open(url);
+      }, 0)
+    } else {
+      alert("Query를 제거해주시기 바랍니다.")
+    }
   };
 
   const editDisabled = !url.includes("?")
