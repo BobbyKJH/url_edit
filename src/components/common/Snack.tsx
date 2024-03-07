18392016faa807ac2bd16cb110a2d9b55ffa75b5
bobby <rlatnguscjsw@gmail.com>
2024-03-07 22:03:23 +0900

    Feat : Snack Component
    - Snack Component Create

--- src/components/common/Snack.tsx
+++ src/components/common/Snack.tsx
@@ -2,6 +2,11 @@ import React from "react";
 /** Style */
 import { Snackbar } from "@mui/material";
 
+/** Recoil 사용 -> props or recoil 바로 사용
+ * Props 사용시 memorization 생각
+ * app.tsx
+ */
+
 const Snack: React.FC = () => {
   return (
     <Snackbar
