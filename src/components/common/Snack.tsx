9631c237cc362bc4bcc24ff1dae066b4f056ca5c
bobby <rlatnguscjsw@gmail.com>
2024-03-11 23:48:37 +0900

    Feat : Snack Component
    - Snack Jotai handleClose FC 생성

--- src/components/common/Snack.tsx
+++ src/components/common/Snack.tsx
@@ -1,24 +1,37 @@
 import React from "react";
 /** Style */
-import { Snackbar } from "@mui/material";
+import { Alert, Snackbar } from "@mui/material";
+import { useAtom } from "jotai";
+import { SnackAtom } from "@/atom/SnackAtom";
 
-/** Recoil 사용 -> props or recoil 바로 사용
+/** Recoil 사용 -> props or jotai 바로 사용
  * Props 사용시 memorization 생각
  * app.tsx
  */
 
 const Snack: React.FC = () => {
+  const [snack, setSnack] = useAtom(SnackAtom);
+
+  const handleClose = () => {
+    setSnack({ ...snack, state: false });
+  };
+
   return (
     <Snackbar
       anchorOrigin={{ vertical: "bottom", horizontal: "right" }}
-      open={true}
-      autoHideDuration={1000}
-      sx={{ backgroundColor: "red"}}
-      security="success"
-      // onClose={handleClose}
-      message="I love snacks"
+      open={snack.state}
+      autoHideDuration={3000}
+      onClose={handleClose}
       key={"vertical" + "horizontal"}
-    />
+    >
+      <Alert
+        severity={snack.severity}
+        variant="filled"
+        sx={{ width: '100%' }}
+      >
+        {snack.message}
+      </Alert>
+    </Snackbar>
   )
 };
 
