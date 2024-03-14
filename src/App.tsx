8b3fa1215c1fdfe12d561c6644b508202f846cfc
bobby <rlatnguscjsw@gmail.com>
2024-03-14 17:14:26 +0900

    Feat : Tailwind css
    - mui -> tailwindcss 변경
    - tailwind css 설치 및 설정

--- src/App.tsx
+++ src/App.tsx
@@ -2,28 +2,25 @@ import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 /** Jotai */
 import { Provider } from "jotai";
+/** Component */
+import Header from "@/components/common/Header";
 /** Page */
 import HomePage from "@/page/HomePage";
 import BrandPage from "@/page/BrandPage";
 /** Style */
-import ResetStyle from "@/App.styled";
-import { StyledEngineProvider } from "@mui/styled-engine";
-import Header from "@/components/common/Header";
+import "@/App.css";
 
 const App: React.FC = () => {
   return (
-    <StyledEngineProvider injectFirst>
-      <ResetStyle/>
-      <Provider>
-        <BrowserRouter>
-          <Header/>
-          <Routes>
-            <Route path="/" element={<HomePage/>}/>
-            <Route path="/brand" element={<BrandPage/>}/>
-          </Routes>
-        </BrowserRouter>
-      </Provider>
-    </StyledEngineProvider>
+    <Provider>
+      <BrowserRouter>
+        <Header/>
+        <Routes>
+          <Route path="/" element={<HomePage/>}/>
+          <Route path="/brand" element={<BrandPage/>}/>
+        </Routes>
+      </BrowserRouter>
+    </Provider>
   )
 };
 
