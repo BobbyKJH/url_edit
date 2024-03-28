b99c82de3058682a26bd1964f020458375a05909
bobby <rlatnguscjsw@gmail.com>
2024-03-28 23:47:47 +0900

    Feat : Recoil Un Install
    - Recoil Root 제거

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,7 @@
 import ReactDOM from 'react-dom/client'
 /** Components */
 import App from '@/App.tsx'
-/** Atom */
-import { RecoilRoot } from 'recoil'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <RecoilRoot>
-    <App />
-  </RecoilRoot>
+  <App />
 )
